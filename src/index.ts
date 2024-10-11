04d15219218d3235c7ecd1e58d8b955f1a864782
404pandas <mary.panda.jackson@gmail.com>
2024-10-11 12:46:37 -0400

    planning to randomly generate data. Need to build out the functions and CLI.ts next so I can test my app.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,54 @@
+// import classes
+// TODO NEXT: BUILD THE CLASSES
+import { Human } from "./classes/Human.js";
+import { Monster } from "./classes/Monster.js";
+import { Horse } from "./classes/Horse.js";
+import CLI from "./classes/Cli.js";
+
+// ** DUMMY DATA TO TEST THE CLASSES ***
+
+// Creates an empty array to store creatures
 const creatures = [];
 
+// Creates a new Human instance
 const witcher = new Human(
-  ClI.generateName(), // Name
-  100, // Health
-  50, // Attack power
-  "Witcher", // Profession
-  30, // Defense
-  10, // Speed
-  ["Swordsmanship", "Alchemy"] // Skills
+  // TODO: BUILD THE randomName METHOD
+  CLI.randomName(), // Random Name
+  CLI.randomHealth(), // Random Health
+  CLI.randomAttackPower(), // Attack power
+  CLI.randomProfession(), // Profession
+  CLI.randomDefense(), // Random Defense
+  CLI.randomSpeed(), // Random Speed
+  CLI.randomSkills() // Random Skills
 );
 
+// Creates a new Monster instance
 const griffin = new Monster(
-  ClI.generateName(), // Dynamic name
-  150, // Health
-  40, // Attack power
-  "Griffin", // Type
-  25, // Defense
-  20 // Speed
+  CLI.randomName(), // Random Name
+  CLI.randomHealth(), // Random Health
+  CLI.randomAttackPower(), // Attack power
+  CLI.randomType(), // Type
+  CLI.randomDefense(), // Random Defense
+  CLI.randomSpeed() // Random Speed
 );
 
+// Creates a new Horse instance
 const roach = new Horse(
-  ClI.generateName(), // Dynamic name
-  80, // Health
-  600, // Carrying capacity
-  15, // Speed
-  10 // Endurance
+  CLI.randomName(), // Random Name
+  CLI.randomHealth(), // Random Health
+  CLI.randomCapacity(), // Carrying capacity
+  CLI.randomSpeed(), // Random Speed
+  CLI.randomEndurance() // Random Endurance
 );
+
+// Pushes the instances to the creatures array
+creatures.push(witcher);
+creatures.push(griffin);
+creatures.push(roach);
+
+// Creates a new CLI instance
+const cli = new CLI(creatures);
+
+// Calls the startCLI method
+// TODO: BUILD THE startCLI METHOD
+cli.startCLI();
